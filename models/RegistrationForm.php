e5db822a1bf4183fd24bcaa1fbca947b3f769c54
Anton <zerolimitss@gmail.com>
2018-05-11 14:17:49 +0300

    security

--- models/RegistrationForm.php
+++ models/RegistrationForm.php
@@ -26,7 +26,6 @@ class RegistrationForm extends Model
             [['username', 'password', 'password_repeat'], 'required'],
             ['password_repeat', 'compare', 'compareAttribute' => 'password'],
             ['username', 'unique', 'targetClass' => 'app\models\User'],
-            ['username', 'unique', 'targetClass' => 'app\models\User'],
         ];
     }
 
